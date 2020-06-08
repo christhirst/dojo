07719b37bcfa706c03174d1fb531760656eaea7a
Sebastian Grollmisch <sebastian@grollmisch.com>
2020-06-08 22:36:31 +0200

    reorg für tempconv0

---
+++ go/src/ch2/ch2.go
@@ -0,0 +1,8 @@
+package main
+
+import "ch2/tempconv0"
+import "fmt"
+
+func main () {
+	fmt.Printf(tempconv0.AbsoluteZeroC)
+}
--- go/src/ch2/tempconv0/tempconv0.go
+++ go/src/ch2/tempconv0/tempconv0.go
@@ -1,4 +1,4 @@
-package tempconv
+package tempconv0
 
 type Celsius float64
 type Fahrenheit float64
