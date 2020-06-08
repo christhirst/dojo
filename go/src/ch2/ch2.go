9bbcc2a8dcf805c467fd81de3b55adff181ca891
Sebastian Grollmisch <sebastian@grollmisch.com>
2020-06-09 00:37:41 +0200

    ending with ch2

--- go/src/ch2/ch2.go
+++ go/src/ch2/ch2.go
@@ -1,8 +1,30 @@
 package main
 
-import "ch2/tempconv0"
-import "fmt"
+import (
+	"ch2/tempconv0"
+	"fmt"
+	"os"
+)
 
-func main () {
+func x() int {
+	return 0
+}
+func main() {
 	fmt.Printf(tempconv0.AbsoluteZeroC.String())
+	if a := x(); a == 0 {
+		fmt.Println(a)
+	} else {
+		//
+	}
+
+	if f, err := os.Create("./myfile"); err != nil {
+		if a, err := f.WriteString("bla"); err != nil {
+			fmt.Println(a)
+		} else {
+			fmt.Println(a)
+		}
+	} else {
+		fmt.Println("blub")
+		fmt.Println(err)
+	}
 }
