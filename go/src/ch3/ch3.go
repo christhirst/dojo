30de2b0cb043ff34e27bb3c1326476e52a9c3161
Sebastian Grollmisch <sebastian@grollmisch.com>
2020-08-30 13:29:30 +0200

    endlich im git repo drin

--- go/src/ch3/ch3.go
+++ go/src/ch3/ch3.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
 /*
@@ -28,4 +29,82 @@ func main() {
 
 	fmt.Printf("%08b\n", x&^y)
 	fmt.Printf("%08b\n", x^y)
+
+	o := 0666
+	fmt.Printf("%d %[1]o %#[1]o\n", o) //438 666 0666
+
+	//var f float32 = 16777216 //true
+	//var f float64 = 16777216 //false
+	var f int32 = 16777216 //false
+	fmt.Println(f)
+	fmt.Println(f + 1)
+	fmt.Println(f == f+1)
+
+	const e = 2.71828
+	/*
+		g --> chooses the most compact representation
+		e --> exponent representation
+		f --> noexponent representation
+	*/
+	fmt.Printf("%[1]g %[1]e %[1]f", e)
+
+	/*
+		IEEE 754
+		negative and positive infinities
+		zero devisioned by zero --> NaN ( not a Number )
+	*/
+	var z float64
+	fmt.Println(z, -z, 1/z, -1/z, z/z)
+
+	fmt.Println(compute())
+
+	myString()
+}
+
+func compute() (float64, bool) {
+	failed := true
+	if failed {
+		return 0, false
+	}
+	return 1.0, true
+}
+
+func myString() {
+	/*
+		len() returns bytes
+		index operand returns the n-th byte, not rune
+	*/
+	s := "hello, worldä"
+	fmt.Println(len(s))            //12
+	fmt.Println(s[0], s[len(s)-1]) //104 164
+	fmt.Println("\164")
+
+	/*
+		c := s[len(s)] // panic
+		fmt.Println(c)
+		... working with utf-8 needs 3 bytes per rune to work with
+	*/
+	fmt.Println(s[0:5])
+	fmt.Println(s[:5])
+
+	//strings are immutable
+	fmt.Println(&s)
+	t := s
+	s = " "
+	fmt.Println(&s)
+	fmt.Println(t)
+	fmt.Println(`\`)
+
+	/*
+		Cannot be assigned ...
+		s[0] = 'L'
+	*/
+
+	s = "hellow world"
+	for i := 0; i < len(s); {
+		r, size := utf8.DecodeRuneInString(s[i:])
+		fmt.Println(r)
+		i += size
+	}
+
 }
